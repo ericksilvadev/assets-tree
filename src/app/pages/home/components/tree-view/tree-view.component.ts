82ff62e03243d4f1e9f40cbfac162a480bf99159
Erick Silva <erick.silva@siteware.com.br>
2024-07-12 03:42:44 -0300

    #tree-view - rendering tree-view with seach filter

---
+++ src/app/pages/home/components/tree-view/tree-view.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { SearchFilterComponent } from "./search-filter/search-filter.component";
+
+@Component({
+  selector: 'app-tree-view',
+  standalone: true,
+  imports: [SearchFilterComponent],
+  templateUrl: './tree-view.component.html',
+  styleUrl: './tree-view.component.scss'
+})
+export class TreeViewComponent {
+
+}
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,11 +4,12 @@ import { Company } from '../../models/company.model';
 import { AppContextService } from '../../services/app-context.service';
 import { BreadcrumbsComponent } from "./components/breadcrumbs/breadcrumbs.component";
 import { FilterComponent } from "./components/filter/filter.component";
+import { TreeViewComponent } from "./components/tree-view/tree-view.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [BreadcrumbsComponent, FilterComponent],
+  imports: [BreadcrumbsComponent, FilterComponent, TreeViewComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
