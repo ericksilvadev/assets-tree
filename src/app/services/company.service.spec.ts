658966917bb07c7f6e2b09c702cd12af5cd04061
Erick Silva <erick.silva@siteware.com.br>
2024-07-11 22:40:36 -0300

    #header - (GREEN) CompanyService - should not set context company if already set & should set context company if not set

--- src/app/services/company.service.spec.ts
+++ src/app/services/company.service.spec.ts
@@ -6,6 +6,7 @@ import { Company } from '../models/company.model';
 import { CompanyRepository } from '../repositories/company.repository';
 import { CompanyService } from './company.service';
 import { Injector, runInInjectionContext } from '@angular/core';
+import { AppContextService } from './app-context.service';
 
 describe('CompanyService', () => {
   let service: CompanyService;
@@ -38,5 +39,40 @@ describe('CompanyService', () => {
     done();
   });
 
-  it('should set ')
+  it('should set context company if not set', () => {
+    // arrange
+    const company = new Company('1', 'Company 1');
+    const context = TestBed.inject(AppContextService);
+    context.currentCompany.next(new Company('', ''));
+
+    spyOn(service['companyRepository'], 'getCompanies').and.returnValue(of([company]));
+
+    // act
+    runInInjectionContext(TestBed.inject(Injector), () => {
+      service.setCompanies()
+    });
+
+    // assert
+    expect(context.currentCompany.value.id).toBe(company.id);
+    expect(context.currentCompany.value.name).toBe(company.name);
+  });
+
+  it('should not set context company if already set', () => {
+    // arrange
+    const company = new Company('1', 'Company 1');
+    const contextCompany = new Company('2', 'Company 2');
+    const context = TestBed.inject(AppContextService);
+    context.currentCompany.next(contextCompany);
+
+    spyOn(service['companyRepository'], 'getCompanies').and.returnValue(of([company]));
+
+    // act
+    runInInjectionContext(TestBed.inject(Injector), () => {
+      service.setCompanies()
+    });
+
+    // assert
+    expect(context.currentCompany.value.id).toBe(contextCompany.id);
+    expect(context.currentCompany.value.name).toBe(contextCompany.name);
+  });
 });
--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,21 +1,28 @@
-import { Injectable, signal, Signal } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { Injectable, signal, WritableSignal } from '@angular/core';
 import { Company } from '../models/company.model';
 import { CompanyRepository } from '../repositories/company.repository';
+import { AppContextService } from './app-context.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyService {
 
-  public companies: Signal<Company[]> = signal([]);
+  public companies: WritableSignal<Company[]> = signal([]);
 
-  constructor(private companyRepository: CompanyRepository) {
+  constructor(private companyRepository: CompanyRepository, private appContext: AppContextService) {
     this.setCompanies();
   }
 
   public setCompanies() {
-    this.companies = toSignal(this.companyRepository.getCompanies(), { initialValue: [] });
+    this.companyRepository.getCompanies()
+      .subscribe((companies) => {
+        this.companies.set(companies);
+
+        if (this.appContext.currentCompany.value.id === '') {
+          this.appContext.setCurrentCompany(companies[0]);
+        }
+      });
   }
 
 }
