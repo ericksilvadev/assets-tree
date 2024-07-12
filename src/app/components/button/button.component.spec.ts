3d3e572c1b65f27316e9fe42c3dac531ad7d0d6c
Erick Silva <erick.silva@siteware.com.br>
2024-07-11 21:05:59 -0300

    #button - creating button component

---
+++ src/app/components/button/button.component.spec.ts
@@ -0,0 +1,37 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ButtonComponent } from './button.component';
+import { By } from '@angular/platform-browser';
+import { IconComponent } from '../icon/icon.component';
+
+describe('ButtonComponent', () => {
+  let component: ButtonComponent;
+  let fixture: ComponentFixture<ButtonComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      imports: [ButtonComponent]
+    })
+      .compileComponents();
+
+    fixture = TestBed.createComponent(ButtonComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should render icon correctly', () => {
+    // arrange
+    component.icon = 'asset';
+
+    // act
+    fixture.detectChanges();
+    const iconComponent = fixture.debugElement.query(By.css('app-icon')).componentInstance as IconComponent;
+
+    // assert
+    expect(iconComponent.name).toBe(component.icon);
+  });
+});
