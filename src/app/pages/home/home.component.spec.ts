76a37c098971573d1674e11ce5376f085af84ea2
Erick Silva <erick.silva@siteware.com.br>
2024-07-12 01:08:54 -0300

    #home - (GREEN) HomeComponent - should show current company on breadcrumbs path

--- src/app/pages/home/home.component.spec.ts
+++ src/app/pages/home/home.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { HomeComponent } from './home.component';
+import { AppContextService } from '../../services/app-context.service';
+import { Company } from '../../models/company.model';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -10,8 +12,8 @@ describe('HomeComponent', () => {
     await TestBed.configureTestingModule({
       imports: [HomeComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(HomeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +21,19 @@ describe('HomeComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should show current company on breadcrumbs path', () => {
+    // arrange
+    const company = new Company('1', 'Company 1');
+    const contextService = TestBed.inject(AppContextService);
+    contextService.currentCompany.next(company);
+
+    // act
+    fixture.detectChanges();
+    const breadcrumbs = fixture.nativeElement.querySelector('app-breadcrumbs');
+
+    // assert
+    expect(breadcrumbs.innerText).toContain(company.name);
   });
 });
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
+import { BreadcrumbsComponent } from "./components/breadcrumbs/breadcrumbs.component";
+import { AppContextService } from '../../services/app-context.service';
+import { Company } from '../../models/company.model';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [BreadcrumbsComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
-export class HomeComponent {
+export class HomeComponent implements OnDestroy {
+  protected selectedCompany: Company = new Company('', '');
 
+  private _companySubscription: Subscription
+
+  constructor(appContext: AppContextService) {
+    this._companySubscription = appContext.currentCompany.subscribe((company) => {
+      this.selectedCompany = company;
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._companySubscription.unsubscribe();
+  }
 }
