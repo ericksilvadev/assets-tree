1ac83c1c590c8f4e0ebf52071ee94f97bdec2c42
Erick Silva <erick.silva@siteware.com.br>
2024-07-12 03:05:50 -0300

    #home - select company by initial route

--- src/app/services/company.service.ts
+++ src/app/services/company.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, signal, WritableSignal } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
 import { map } from 'rxjs';
 import { Company } from '../models/company.model';
 import { CompanyRepository } from '../repositories/company.repository';
@@ -11,8 +12,20 @@ export class CompanyService {
 
   public companies: WritableSignal<Company[]> = signal([]);
 
-  constructor(private companyRepository: CompanyRepository, private appContext: AppContextService) {
+  private companyParamId?: string;
+
+  constructor(private companyRepository: CompanyRepository, private appContext: AppContextService, private router: Router) {
+    this.setCompanyIdByRoute();
     this.setCompanies();
+  }
+
+  private setCompanyIdByRoute() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const companyId = event.url.split('/').pop();
+        this.setCompanyParamId(companyId ?? '');
+      }
+    });
   }
 
   public setCompanies() {
@@ -21,7 +34,7 @@ export class CompanyService {
       .subscribe((companies) => {
         this.companies.set(companies);
 
-        this.setCurrentCompanyIfNotSet(companies[0]);
+        this.setCompanyById();
       });
   }
 
@@ -29,10 +42,14 @@ export class CompanyService {
     return companies.map((company) => new Company(company.id, company.name + ' Unit'));
   }
 
-  private setCurrentCompanyIfNotSet(company: Company) {
-    if (this.appContext.currentCompany.value.id === '') {
-      this.appContext.setCurrentCompany(company);
-    }
+  private setCompanyById() {
+    const company = this.companies().find((company) => company.id === this.companyParamId);
+
+    this.appContext.setCurrentCompany(company ?? this.companies()[0]);
+  }
+
+  public setCompanyParamId(companyId: string) {
+    this.companyParamId = companyId;
   }
 
 }
