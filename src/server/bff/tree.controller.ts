446474171036c1a25f9cf0832ba9bcea170f8b3d
Erick Silva <erick.silva@siteware.com.br>
2024-07-15 17:07:35 -0300

    #tree-view - creating tree-items controller

---
+++ src/server/bff/tree.controller.ts
@@ -0,0 +1,17 @@
+import { Request, Response } from 'express';
+import { GetTreeItemsService } from '../domain/useCases/get-tree-items.service';
+
+export class TreeController {
+  constructor(private getTreeItemsService: GetTreeItemsService) { }
+
+  async getTreeItems(req: Request, res: Response) {
+    const { id } = req.params;
+
+    try {
+      const treeItems = await this.getTreeItemsService.getItems(id);
+      res.send(treeItems);
+    } catch (error: unknown) {
+      res.status(500).json({ error });
+    }
+  }
+}
