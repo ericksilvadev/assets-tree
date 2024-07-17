2623045d3b5ff12fb207ea9d91647b400a0b5893
Erick Silva <erick.silva@siteware.com.br>
2024-07-17 00:56:30 -0300

    #tree-view - (GREEN) ComponentDetailsComponent - should display component name

--- src/app/pages/home/components/component-details/component-details.component.spec.ts
+++ src/app/pages/home/components/component-details/component-details.component.spec.ts
@@ -1,23 +1,36 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ComponentDetailsComponent } from './component-details.component';
+import { TreeItemType } from '../tree-view/tree-item/models/tree-item.enum';
+import { TreeItemModel } from '../tree-view/tree-item/models/tree-item.model';
 
 describe('ComponentDetailsComponent', () => {
   let component: ComponentDetailsComponent;
+  let componentItem: TreeItemModel;
   let fixture: ComponentFixture<ComponentDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [ComponentDetailsComponent]
     })
-    .compileComponents();
-    
+      .compileComponents();
+
     fixture = TestBed.createComponent(ComponentDetailsComponent);
     component = fixture.componentInstance;
+    componentItem = new TreeItemModel('1', 'Component', TreeItemType.Component, null, null);
+    component['component'].set(componentItem);
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should display component name', () => {
+    // arrange
+    const title = fixture.debugElement.nativeElement.querySelector('h1.component-title');
+
+    // assert
+    expect(title.textContent).toEqual(componentItem.name);
   });
 });
--- src/app/pages/home/components/component-details/component-details.component.ts
+++ src/app/pages/home/components/component-details/component-details.component.ts
@@ -1,4 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, signal } from '@angular/core';
+import { TreeItemModel } from '../tree-view/tree-item/models/tree-item.model';
+import { TreeItemType } from '../tree-view/tree-item/models/tree-item.enum';
+import { ComponentService } from '../../../../services/component.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-component-details',
@@ -7,6 +11,17 @@ import { Component } from '@angular/core';
   templateUrl: './component-details.component.html',
   styleUrl: './component-details.component.scss'
 })
-export class ComponentDetailsComponent {
+export class ComponentDetailsComponent implements OnDestroy {
+  protected component = signal(new TreeItemModel('', '', TreeItemType.Component, null, null));
+  private _componentSubscription: Subscription;
 
+  constructor(componentService: ComponentService) {
+    this._componentSubscription = componentService.selectedComponent.subscribe((component) => {
+      this.component.set(component);
+    });
+  }
+
+  ngOnDestroy() {
+    this._componentSubscription.unsubscribe();
+  }
 }
