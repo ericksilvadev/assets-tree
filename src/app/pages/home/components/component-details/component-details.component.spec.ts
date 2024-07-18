92410f36b4f28159ccfab4c66b2941b6e57e1265
Erick Silva <erick.silva@siteware.com.br>
2024-07-17 21:24:47 -0300

    #tree-view - (GREEN) ComponentDetailsComponent - should get component details when selected component changes

--- src/app/pages/home/components/component-details/component-details.component.spec.ts
+++ src/app/pages/home/components/component-details/component-details.component.spec.ts
@@ -3,21 +3,26 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { ComponentDetailsComponent } from './component-details.component';
 import { TreeItemType } from '../tree-view/tree-item/models/tree-item.enum';
 import { TreeItemModel } from '../tree-view/tree-item/models/tree-item.model';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { ComponentModel } from '../../../../models/component.model';
+import { Sensors } from '../../../../models/sensors.enum';
+import { Status } from '../../../../models/status.enum';
+import { of } from 'rxjs';
 
 describe('ComponentDetailsComponent', () => {
   let component: ComponentDetailsComponent;
-  let componentItem: TreeItemModel;
+  let componentItem: ComponentModel;
   let fixture: ComponentFixture<ComponentDetailsComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ComponentDetailsComponent]
+      imports: [ComponentDetailsComponent, HttpClientTestingModule]
     })
       .compileComponents();
 
     fixture = TestBed.createComponent(ComponentDetailsComponent);
     component = fixture.componentInstance;
-    componentItem = new TreeItemModel('1', 'Component', TreeItemType.Component, null, null);
+    componentItem = new ComponentModel('1', 'Component', '1', Status.Operating, Sensors.Energy, '1', '1', '1');
     component['component'].set(componentItem);
     fixture.detectChanges();
   });
@@ -32,5 +37,19 @@ describe('ComponentDetailsComponent', () => {
 
     // assert
     expect(title.textContent).toEqual(componentItem.name);
+  });
+
+  it('should get component details when selected component changes', () => {
+    // arrange
+    const selectedItem = new TreeItemModel('2', 'Component', TreeItemType.Component, '2', Status.Alert, Sensors.Energy);
+    const componentDetails = new ComponentModel(selectedItem.id, selectedItem.name, '1', selectedItem.status!, selectedItem.sensor!, '1', selectedItem.parentId!, null);
+    const spyGetComponent = spyOn(component['componentService'], 'getComponent').and.returnValue(of(componentDetails));
+
+    // act
+    component['componentService'].setSelectedComponent(selectedItem);
+
+    // assert
+    expect(spyGetComponent).toHaveBeenCalledWith(selectedItem.id);
+    expect(component['component']()).toEqual(componentDetails);
   });
 });
--- src/app/pages/home/components/component-details/component-details.component.ts
+++ src/app/pages/home/components/component-details/component-details.component.ts
@@ -3,6 +3,9 @@ import { TreeItemModel } from '../tree-view/tree-item/models/tree-item.model';
 import { TreeItemType } from '../tree-view/tree-item/models/tree-item.enum';
 import { ComponentService } from '../../../../services/component.service';
 import { Subscription } from 'rxjs';
+import { ComponentModel } from '../../../../models/component.model';
+import { Sensors } from '../../../../models/sensors.enum';
+import { Status } from '../../../../models/status.enum';
 
 @Component({
   selector: 'app-component-details',
@@ -12,13 +15,21 @@ import { Subscription } from 'rxjs';
   styleUrl: './component-details.component.scss'
 })
 export class ComponentDetailsComponent implements OnDestroy {
-  protected component = signal(new TreeItemModel('', '', TreeItemType.Component, null, null));
+  protected component = signal(new ComponentModel('1', 'Component', '1', Status.Operating, Sensors.Energy, '1', '1', '1'));
   private _componentSubscription: Subscription;
 
-  constructor(componentService: ComponentService) {
+  constructor(private componentService: ComponentService) {
     this._componentSubscription = componentService.selectedComponent.subscribe((component) => {
-      this.component.set(component);
+      this.setComponent(component);
     });
+  }
+
+  private setComponent(component: TreeItemModel): void {
+    if (component.type === TreeItemType.Component) {
+      this.componentService.getComponent(component.id).subscribe((componentDetails: ComponentModel) => {
+        this.component.set(componentDetails);
+      });
+    }
   }
 
   ngOnDestroy() {
